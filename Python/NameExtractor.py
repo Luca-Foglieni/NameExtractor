cfba030585e6dcb2f289749bf2cd488af1bd6268
Luca Foglieni <luca.foglieni02@gmail.com>
2024-09-16 16:06:36 +0200

    Update NameExtractor.py

--- Python/NameExtractor.py
+++ Python/NameExtractor.py
@@ -5,14 +5,14 @@ names = open("../names.txt", "r")
 list = names.readlines()
 
 try:
-    extraction = sample(list, len(list))
+	extraction = sample(list, len(list))
 
-    print("Press enter to extract itmes one by one.")
+	print("Press enter to extract itmes one by one.")
 
-    for name in extraction:
-        input()
-        print(name)
+	for name in extraction:
+		input()
+		print(name)
 
-    print("Every item has been extracted.")
+	print("Every item has been extracted.")
 except ValueError:
-    print("The name list is empty. Populate it before running the extraction.")
+	print("The name list is empty. Populate it before running the extraction.")
